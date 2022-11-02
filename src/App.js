19f997c6f8ff3fcbbf0364c61e98451c844a4ba5
mohamed <immohamedsaad@gmail.com>
2022-11-02 17:46:03 +0200

    make rusable component of news section

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
 import { Overview } from "./design-system/Components/Overview/Overview";
-import {
-  faUserGroup,
-  faMicrophone,
-  faMap,
-} from "@fortawesome/free-solid-svg-icons";
-
+import DataView from "./design-system/Components/Overview/Data";
 function App() {
   return (
     <div className="App">
-      <Overview
-        icon={faUserGroup}
-        title="650 Participant"
-        Children=" Quisque libero sapien. Integer tellus nisal, efficitur sed dolor at,
-      vehicula finibus massa. sed tincidunt metus sed eleifend suscipit"
-      />
-      <Overview
-        icon={faMap}
-        title="24 Program"
-        Children=" Quisque libero sapien. Integer tellus nisal, efficitur sed dolor at,
-      vehicula finibus massa. sed tincidunt metus sed eleifend suscipit"
-      />
-      <Overview
-        icon={faMicrophone}
-        title="11 Speakers"
-        Children=" Quisque libero sapien. Integer tellus nisal, efficitur sed dolor at,
-      vehicula finibus massa. sed tincidunt metus sed eleifend suscipit"
-      />
+      <div style={{ display: "flex" }}>
+        {DataView.map((item) => {
+          return (
+            <Overview
+              icon={item.icon}
+              title={item.title}
+              Children={item.description}
+              key={item.id}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/design-system/Components/Overview/Data.jsx
@@ -0,0 +1,29 @@
+import {
+  faUserGroup,
+  faMicrophone,
+  faMap,
+} from "@fortawesome/free-solid-svg-icons";
+const dataView = [
+  {
+    id: 1,
+    icon: faUserGroup,
+    title: "650 Participant",
+    description:
+      " Quisque libero sapien. Integer tellus nisal, efficitur sed dolor atvehicula finibus massa. sed tincidunt metus sed eleifend suscipit",
+  },
+  {
+    id: 2,
+    icon: faMicrophone,
+    title: "24 Program",
+    description:
+      " Quisque libero sapien. Integer tellus nisal, efficitur sed dolor atvehicula finibus massa. sed tincidunt metus sed eleifend suscipit",
+  },
+  {
+    id: 1,
+    icon: faMap,
+    title: "11 Speakers",
+    description:
+      " Quisque libero sapien. Integer tellus nisal, efficitur sed dolor atvehicula finibus massa. sed tincidunt metus sed eleifend suscipit",
+  },
+];
+export default dataView;
